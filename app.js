30b86a8bd0fdf203f6a7f91c0f3a840ffd6d056b
Tim Koerner <tdmkoerner@gmail.com>
2022-11-03 19:37:15 -0500

    Animating ball

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-//alert("connected");
+let $ball = $("#animateme");
+$ball
+.velocity({width: "500px", height: "500px", fontSize:"75px"},{duration: 3000, easing: "easeInQuad", delay: 1000})
+.velocity("reverse", {duration: 3000, easing: "easeInQuad"})
+.velocity({rotate: "45deg"}, {duration:2000, easing: "easeInQuad"})
+
