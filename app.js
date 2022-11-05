f74349457297f4e872eea6b1ddb2acf11a929c0e
Tim Koerner <tdmkoerner@gmail.com>
2022-11-04 21:58:36 -0500

    apps.js - added transforms

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ var $ball2 = $("#ball2");
 $ball2.velocity({
 	transform: [ "rotate(360deg)", "rotate(0deg)"], // Uses "spring"
 	width: [ "110px", [ 250, 15 ] ], // Uses custom spring physics
-	height: ["110px",[ 0.17, 0.67, 0.83, 0.67 ]]// Defaults to easeInSine, the call's default easing
+	height: ["110px",[ 0.17, 0.67, 0.83, 0.67 ]],
+	backgroundColor: [ "rgba(255,255,12,1.0)" ],
+	
 }, {
 	easing: "easeInSine" // Default easing
 });
@@ -14,6 +16,6 @@ let el = document.getElementById("ball1");
 el.addEventListener("click", function scroller1(){
 	$("html").velocity(
 		{scrollTop: "500px"},
-		{duration: 2000, easing: [50,5]}
+		{duration: 3000, easing: [50,5]}
 	);
 });
