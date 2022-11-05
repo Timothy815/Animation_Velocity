c37eeb9bcae5507f6373b25207eff730ace1c243
Tim Koerner <tdmkoerner@gmail.com>
2022-11-04 21:10:18 -0500

    added scroll attached to ball1
    https://github.com/julianshapiro/velocity/wiki/Option---Easing

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-let $ball1 = $("#ball1");
-$ball1
-.velocity({width: "110px", height: "110px", fontSize:"15px", transform: ['rotate(360deg)','rotate(0deg)']},{duration: 1000, easing: "swing", delay: 500,loop:10});
+var $ball1 = $("#ball1");
+$ball1.velocity({width: "100px", height: "100px", fontSize:"15px", transform: ['rotate(360deg)','rotate(0deg)']},{duration: 1000, easing: "swing", delay: 500,loop:10});
 
-let $ball2 = $("#ball2");
-$ball2
-.velocity({opacity: 0},{duration:1000, easing: "swing"},{display:"none"})
-.velocity("reverse",{duration: 1000, display: "inline-block", loop:10});
+var $ball2 = $("#ball2");
+$ball2.velocity({
+	transform: [ "rotate(360deg)", "rotate(0deg)"], // Uses "spring"
+	width: [ "110px", [ 250, 15 ] ], // Uses custom spring physics
+	height: "110px" // Defaults to easeInSine, the call's default easing
+}, {
+	easing: "easeInSine" // Default easing
+});
 
-
-
-
+let el = document.getElementById("ball1");
+el.addEventListener("click", function scroller1(){
+	$("html").velocity(
+		{scrollTop: "500px"},
+		{duration: 1000, easing: [50,5]}
+	);
+});
