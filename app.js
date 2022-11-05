9ff21f79f0adcdef45c9cf97aa2e9ba09c855b4d
Tim Koerner <tdmkoerner@gmail.com>
2022-11-04 21:16:44 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var $ball2 = $("#ball2");
 $ball2.velocity({
 	transform: [ "rotate(360deg)", "rotate(0deg)"], // Uses "spring"
 	width: [ "110px", [ 250, 15 ] ], // Uses custom spring physics
-	height: "110px" // Defaults to easeInSine, the call's default easing
+	height: ["110px",[ 0.17, 0.67, 0.83, 0.67 ]]// Defaults to easeInSine, the call's default easing
 }, {
 	easing: "easeInSine" // Default easing
 });
@@ -14,6 +14,6 @@ let el = document.getElementById("ball1");
 el.addEventListener("click", function scroller1(){
 	$("html").velocity(
 		{scrollTop: "500px"},
-		{duration: 1000, easing: [50,5]}
+		{duration: 2000, easing: [50,5]}
 	);
 });
